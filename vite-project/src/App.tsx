944c10129e1007f6afa020d949a80893a884887f
fs-projects <fs.projectsuk@gmail.com>
2023-11-10 16:16:24 +0530

    MAINTENANCE

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { Home } from './components/Home.tsx'
-import { About } from './components/About.tsx'
+// import { About } from './components/About.tsx'
 import { Navbar } from './components/Navbar.tsx'
 import { OrderSummary } from './components/OrderSummary.tsx'
 import { NewProducts } from './components/NewProducts.tsx'
