88d959dffb3636ce8b0c72a26194e59e40efd909
fs-projects <fs.projectsuk@gmail.com>
2023-11-10 16:54:22 +0530

    MAINTENANCE

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -5,4 +5,16 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   clearScreen: false,
+  css: {
+    devSourcemap: true
+  },
+  server:{
+    port: 3000,
+    // open: "index.html" - vite will open this file when server starts
+  },
+  preview:{
+    port: 3001,
+    // strictPort: true - enable this only when you want only port 3001 for preview
+  },
+  envDir: "viteenvdir"
 })
